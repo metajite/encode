1c5f0d86a79b0bca0bd96f4c4011a01e285ce697
Daniel <jite.daniel@gmail.com>
2021-11-22 17:40:53 +0000

    homework 5 draft 1

--- VolcanoCoin.sol
+++ VolcanoCoin.sol
@@ -2,12 +2,12 @@
 
 pragma solidity ^0.8.0;
 
-contract VolcanoCoin{
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    uint totalSupply = 10000;
-    address owner;
-    mapping (address => uint) public balance;
-    mapping (address => Payment[]) public userPaymentsMap;
+contract VolcanoCoin is ERC20, Ownable{
+
+    mapping (address => Payment[]) public userPaymentRecords;
     
     struct Payment{
         uint transferAmount;
@@ -18,43 +18,28 @@ contract VolcanoCoin{
     event NewTotalSupply(uint _newTotal);
     event PaymentArrayContent(uint _index, uint _amount);
 
-    constructor(){
-        owner = msg.sender;
-        balance[owner] = totalSupply;
+    constructor(uint initialSupply) ERC20("VolcanoCoin", "VLC"){
+        _mint(msg.sender, initialSupply);
     }
     
-    modifier onlyOwner(){
-        if (msg.sender == owner){
-            _;
-        }
+    function mintMore(uint _additionalSupply) public{
+        _mint(msg.sender, _additionalSupply);
     }
-
-    function getTotalSupply() public view returns (uint){
-        return totalSupply;
-    }
-
-    function increaseTotalSupply() public onlyOwner{
-        totalSupply += 1000;
-        emit NewTotalSupply(totalSupply);
-    }
-
+    
     function transerToken(uint amountToTransfer, address recipientAddress) public {
-        
-        balance[msg.sender] -= amountToTransfer;
-        balance[recipientAddress] += amountToTransfer;
         
         Payment memory _payment;
         _payment.transferAmount = amountToTransfer;
         _payment.recipientAddress = recipientAddress;
         
-        userPaymentsMap[msg.sender].push(_payment);
+        userPaymentRecords[msg.sender].push(_payment);
 
         emit TransferredAmount("Transferred some tokens between accounts", amountToTransfer, recipientAddress);
         
-        uint length = userPaymentsMap[msg.sender].length;
+        uint length = userPaymentRecords[msg.sender].length;
         
         for (uint index=0; index < length; index++){
-            uint val = userPaymentsMap[msg.sender][index].transferAmount;
+            uint val = userPaymentRecords[msg.sender][index].transferAmount;
             emit PaymentArrayContent(index, val);    
         }
         
