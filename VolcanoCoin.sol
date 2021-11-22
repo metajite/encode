cfd2a33325ec2f582db2b8ab4fc6b37e1b85d61b
Daniel <jite.daniel@gmail.com>
2021-11-22 07:39:28 +0000

    homework 4

--- VolcanoCoin.sol
+++ VolcanoCoin.sol
@@ -6,7 +6,23 @@ contract VolcanoCoin{
 
     uint totalSupply = 10000;
     address owner;
+    mapping (address => uint) public balance;
+    mapping (address => Payment[]) public userPaymentsMap;
+    
+    struct Payment{
+        uint transferAmount;
+        address recipientAddress;
+    }
+    
+    event TransferredAmount (string _msg, uint _transferredAmount, address _recipientAddress);
+    event NewTotalSupply(uint _newTotal);
+    event PaymentArrayContent(uint _index, uint _amount);
 
+    constructor(){
+        owner = msg.sender;
+        balance[owner] = totalSupply;
+    }
+    
     modifier onlyOwner(){
         if (msg.sender == owner){
             _;
@@ -19,5 +35,28 @@ contract VolcanoCoin{
 
     function increaseTotalSupply() public onlyOwner{
         totalSupply += 1000;
+        emit NewTotalSupply(totalSupply);
+    }
+
+    function transerToken(uint amountToTransfer, address recipientAddress) public {
+        
+        balance[msg.sender] -= amountToTransfer;
+        balance[recipientAddress] += amountToTransfer;
+        
+        Payment memory _payment;
+        _payment.transferAmount = amountToTransfer;
+        _payment.recipientAddress = recipientAddress;
+        
+        userPaymentsMap[msg.sender].push(_payment);
+
+        emit TransferredAmount("Transferred some tokens between accounts", amountToTransfer, recipientAddress);
+        
+        uint length = userPaymentsMap[msg.sender].length;
+        
+        for (uint index=0; index < length; index++){
+            uint val = userPaymentsMap[msg.sender][index].transferAmount;
+            emit PaymentArrayContent(index, val);    
+        }
+        
     }
 }
